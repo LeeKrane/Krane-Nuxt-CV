57013782a0af3040dfe370e4de1bfb18273619eb
krane <christian.kranabetter@protonmail.com>
2024-08-01 11:31:38 +0200

    fix uneven project amount being displayed to the left first

--- components/Section/Projects.vue
+++ components/Section/Projects.vue
@@ -6,7 +6,7 @@ const { projects } = defineProps<{ projects: ProjectDTO[] }>()
 	<SectionGeneral class="masonry-listing" title="Projects">
 		<div class="ul-container">
 			<ul>
-				<li v-for="project in projects.slice(0, Math.floor(projects.length * 0.5))">
+				<li v-for="project in projects.slice(0, Math.ceil(projects.length * 0.5))">
 					<a :href="project.url" target="_blank"><h2>{{ project.name }}</h2></a>
 					<p>{{ project.description }}</p>
 					<div v-if="project.techStack" class="flex gap-1 mt-2">
@@ -19,7 +19,7 @@ const { projects } = defineProps<{ projects: ProjectDTO[] }>()
 				</li>
 			</ul>
 			<ul>
-				<li v-for="project in projects.slice(Math.floor(projects.length * 0.5))">
+				<li v-for="project in projects.slice(Math.ceil(projects.length * 0.5))">
 					<a :href="project.url" target="_blank"><h2>{{ project.name }}</h2></a>
 					<p>{{ project.description }}</p>
 					<div v-if="project.techStack" class="flex gap-1 mt-2">
